1031dacd5f9164839b3bc8b4b201319e43e8c229
BT_1N3_03@BKTECH.LOCAL <BT_1N3_03@BKTECH.LOCAL>
2018-10-01 08:30:43 -0400

    commit and push

---
+++ src/Roma/Main.java
@@ -0,0 +1,8 @@
+package Roma;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/Roma/RCooperLib.java
@@ -0,0 +1,14 @@
+package Roma;
+
+public class RCooperLib {
+    public static String reverse(String input)
+    {
+        String output = "";
+        int i = input.length() - 1;
+        while (i >=0) {
+            output = output + input.substring(i,i+1);
+            i--;
+        }
+        return output;
+    }
+}
