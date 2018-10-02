6692b8fb7886c5a69bf4788059f71506a45caacf
rcooper3307 <rcooper3307@bths.edu>
2018-10-02 08:21:35 -0400

    bingo

--- src/Roma/Main.java
+++ src/Roma/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("reverse of \'reverse\)
+        System.out.println("reverse of \'reverse\' is "+RCooperLib.reverse( "reverse"));
     }
 }
